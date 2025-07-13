33b8b3e734cfbbf3c965d12ec2d3df32dd1e8317
vijay-2005 <vkprajapati072@gmai.com>
2025-07-13 17:23:09 +0530

    minor changes

--- mcp-chatgpt-responses/gemini_server.py
+++ mcp-chatgpt-responses/gemini_server.py
@@ -270,4 +270,4 @@ if __name__ == "__main__":
     # Get port from environment (Render sets $PORT)
     port = int(os.environ.get("PORT", 8080))
     # Run the server on the specified port
-    mcp.run(transport='streamable-http' ,host="0.0.0.0")
+    mcp.run(transport='streamable-http')
